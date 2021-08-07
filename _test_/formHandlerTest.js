6625abdd5d9561ecdc85945a0de2c9bcb30237db
iprincipe <56736744+iprincipe@users.noreply.github.com>
2021-08-07 13:29:27 -0500

    Delete formHandlerTest.js

--- _test_/formHandlerTest.js
+++
@@ -1,6 +0,0 @@
-import { handleSubmit } from "../src/client/js/formHandler"
-
-describe("Testing the submit function", () => {  
-    test("Testing the handleSubmit() function", () => {
-           expect(handleSubmit).toBeDefined();
-})});
